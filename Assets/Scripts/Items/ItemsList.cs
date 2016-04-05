37ead40a193c691d9250f26f75710ba20fd37e5c
Kasey Keahey <Kasey_keahey@yahoo.com>
2016-04-04 23:31:40 -0500

    removed the test item since it couldnt be run since its not in the scene

--- Assets/Scripts/Items/ItemsList.cs
+++ Assets/Scripts/Items/ItemsList.cs
@@ -5,8 +5,4 @@ using System.Collections.Generic;
 public class ItemsList : MonoBehaviour {
 	static public List<Item> Items = new List<Item> ();
 
-	void Main () {
-		ItemsList.Items.Add (new Item { IDNum = 0 + ItemsList.Count, Name = Test, Volume = 2, Mass = 2 });
-	}
-
 }
