d06818cf63d3c0b217cc966252485172e729fc3b
Kasey Keahey <Kasey_keahey@yahoo.com>
2016-04-04 23:32:47 -0500

    Changed it so it only will add an entry in the Master list if there is no other dupicate entry

--- Assets/Scripts/Resources/ResourceNode.cs
+++ Assets/Scripts/Resources/ResourceNode.cs
@@ -8,7 +8,12 @@ public class ResourceNode : MonoBehaviour {
 	public int ResourceMas;
 
 	void Start () {
-		ItemsList.Items.Add (new Item { IDNum = 0 + ItemsList.Count, Name = ResourceType, Volume = ResourceVol, Mass = ResourceMas });
+		if (ItemsList.Items.Contains(new Item {Name = ResourceType})) {
+			Debug.Log ("There is already an item that has this name");
+		} else{
+			Debug.Log ("Added Item to List");
+			ItemsList.Items.Add (new Item { ItemID = ItemsList.Items.Count, Name = ResourceType, Volume = ResourceVol, Mass = ResourceMas });
+		}
 	}
 	
 	// Update is called once per frame
