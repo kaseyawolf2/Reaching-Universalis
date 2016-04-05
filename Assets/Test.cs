b3fd67ff07209d9301262dfda04b0e3f878d4938
Kasey Keahey <Kasey_keahey@yahoo.com>
2016-04-04 23:33:11 -0500

    DEbug info Added

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour {
+
+
+
+	void Main () {
+		ItemsList.Items.Add (new Item { ItemID = ItemsList.Items.Count, Name = "Test", Volume = 2, Mass = 2 });
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown("r")){
+			foreach (Item c in ItemsList.Items)
+				Debug.Log (c);
+		}
+		if(Input.GetKeyDown("e")){
+			Debug.Log ("Contains(\"Name = Test\"): " + ItemsList.Items.Contains(new Item {Name = "Test"}));
+				
+		}
+
+	}
+}
