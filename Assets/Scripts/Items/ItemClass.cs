d4bbfd79a35465b7b02ad6177d6e882812a57c5b
Kasey Keahey <Kasey_keahey@yahoo.com>
2016-04-04 23:30:58 -0500

    Changed the ID Name, Changed how strings are writen, added a way to check whats in the master list

--- Assets/Scripts/Items/ItemClass.cs
+++ Assets/Scripts/Items/ItemClass.cs
@@ -1,14 +1,38 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 
-public class Item {
-	public int IDNum { get; set; }
+public class Item : IEquatable<Item> {
+	public int ItemID { get; set; }
 	
 	public string Name { get; set; }
 
 	public float Volume { get; set; }
 
 	public float Mass { get; set; }
+
+	public override string ToString()
+	{
+		return "ID #: " + ItemID + " Name: " + "'" + Name + "'" + " Volume: " + Volume + " Mass: " + Mass;
+	}
+
+	public override int GetHashCode()
+	{
+		return ItemID;
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (obj == null) return false;
+		Item objAsPart = obj as Item;
+		if (objAsPart == null) return false;
+		else return Equals(objAsPart);
+	}
+
+	public bool Equals(Item other)
+	{
+		if (other == null) return false;
+		return (this.Name.Equals(other.Name));
+	}
 }
 
