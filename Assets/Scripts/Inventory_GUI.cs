41d1c10212b764c53cdceef6f2703eb4b59daa30
Kasey Keahey <Kasey_keahey@yahoo.com>
2016-04-23 12:57:22 -0500

    Renamed PageNum So i can split the two apart

--- Assets/Scripts/Inventory_GUI.cs
+++ Assets/Scripts/Inventory_GUI.cs
@@ -8,7 +8,7 @@ public class Inventory_GUI : MonoBehaviour {
 	//List Formating
 	float ListOffset;
 	int MaxNumEntrys;
-	public int PageNum = 1;
+	public int PageNumPlr = 1;
 	public int MaxPage;
 	//List Declaerations
 	List<Item> HeldItem;
@@ -47,16 +47,16 @@ public class Inventory_GUI : MonoBehaviour {
 				//Up
 			if(GUI.Button (new Rect ((Screen.width / 2) - 25 - InventoryBevel * 3, (Screen.height / 6) + InventoryBevel * 3, 25, 25),"^")){
 				MaxPage = HeldItem.Count / MaxNumEntrys;
-				if (PageNum > 0 ) {
-					PageNum -= 1;
+				if (PageNumPlr > 0 ) {
+					PageNumPlr -= 1;
 				}
 			}
 				//Down
 			if(GUI.Button (new Rect ((Screen.width / 2) - 25 - InventoryBevel * 3, ((Screen.height / 1.5f) - InventoryBevel*2)+(Screen.height / 6)-InventoryBevel*6, 25, 25),"v")){
 				
 				MaxPage = HeldItem.Count / MaxNumEntrys;
-				if(PageNum < MaxPage){
-					PageNum += 1;
+				if(PageNumPlr < MaxPage){
+					PageNumPlr += 1;
 				}
 			}
 			//Target
@@ -76,12 +76,12 @@ public class Inventory_GUI : MonoBehaviour {
 				int Count = 0;
 				//For each item display a line
 				foreach (Item C in HeldItem) {
-				if (Count - ((1+MaxNumEntrys)*PageNum) <= MaxNumEntrys) {
-					if( (((Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2)-((PageNum*(MaxNumEntrys+1))*FontHeight)-25 > ((Screen.height / 6) + InventoryBevel * 3)) || (((Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2)+((PageNum*(MaxNumEntrys+1))*FontHeight) < (((Screen.height / 1.5f) - InventoryBevel*2)+(Screen.height / 6)-InventoryBevel*6 - FontHeight*1) )  ){
+				if (Count - ((1+MaxNumEntrys)*PageNumPlr) <= MaxNumEntrys) {
+					if( (((Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2)-((PageNumPlr*(MaxNumEntrys+1))*FontHeight)-25 > ((Screen.height / 6) + InventoryBevel * 3)) || (((Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2)+((PageNumPlr*(MaxNumEntrys+1))*FontHeight) < (((Screen.height / 1.5f) - InventoryBevel*2)+(Screen.height / 6)-InventoryBevel*6 - FontHeight*1) )  ){
 						//Display Items
-						GUI.Label (new Rect ((Screen.width / 6) + InventoryBevel * 2, (Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2 - ((PageNum*(MaxNumEntrys+1))*FontHeight), (Screen.width / 3) - InventoryBevel * 2, FontHeight), Count +" "  + C.ToString ());
+						GUI.Label (new Rect ((Screen.width / 6) + InventoryBevel * 2, (Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2 - ((PageNumPlr*(MaxNumEntrys+1))*FontHeight), (Screen.width / 3) - InventoryBevel * 2, FontHeight), Count +" "  + C.ToString ());
 						//Transfer Arrows Dont Do anything
-						GUI.Button (new Rect ((Screen.width / 2) - 25 - InventoryBevel * 3, (Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2 - ((PageNum*(MaxNumEntrys+1))*FontHeight), FontHeight, FontHeight), ">");
+						GUI.Button (new Rect ((Screen.width / 2) - 25 - InventoryBevel * 3, (Screen.height / 6) + (FontHeight * (Count+2)) + InventoryBevel * 2 - ((PageNumPlr*(MaxNumEntrys+1))*FontHeight), FontHeight, FontHeight), ">");
 					}
 				}
 				//add one to the count so we know how much to displace the GUI.Label and know when to stop
