e9b276a5a7d30ec7eb7f7aaa9c1634fd7c4f705c
kaseyawolf2 <Kasey_keahey@yahoo.com>
2016-11-25 23:48:49 -0600

    Update Settings.cs

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Settings : MonoBehaviour {
+
 	// Use this for initialization
-<<<<<<< HEAD
-	void Awake () {
-		Random.InitState(Statics.Seed);
-=======
 	void Start () {
         Random.InitState(Statics.Seed);
->>>>>>> origin/master
 		if (Statics.ImportFinished == false)
 		{
-            Debug.LogError("Swaping to Loading Scene");
 			SceneManager.LoadScene("Loading");
 		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
 	}
 }
